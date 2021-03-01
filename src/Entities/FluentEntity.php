a7eba6508c95f6b464613696fb7590549811f7e5
MGatner <mgatner@icloud.com>
2021-03-01 03:09:49 +0000

    Add Fluent factory

---
+++ src/Entities/FluentEntity.php
@@ -0,0 +1,76 @@
+<?php namespace Tatter\Users\Entities;
+
+use Fluent\Auth\Entities\User;
+use Tatter\Users\UserEntity;
+
+/**
+ * Fluent User Entity
+ */
+class FluentEntity extends User implements UserEntity
+{
+	/**
+	 * Returns the name of the column used to
+	 * uniquely identify this user, typically 'id'.
+	 *
+	 * @return string
+	 */
+	public function getIdentifier(): string
+	{
+		return $this->getAuthIdColumn();
+	}
+
+	/**
+	 * Returns the value for the identifier,
+	 * or `null` for "uncreated" users.
+	 *
+	 * @return string|int|null
+	 */
+	public function getId()
+	{
+		return $this->getAuthId();
+	}
+
+	/**
+	 * Returns the email address.
+	 *
+	 * @return string|null
+	 */
+	public function getEmail(): ?string
+	{
+		return $this->getAuthEmail();
+	}
+
+	/**
+	 * Returns the username.
+	 *
+	 * @return string|null
+	 */
+	public function getUsername(): ?string
+	{
+		return $this->attributes['username'] ?? null;
+	}
+
+	/**
+	 * Returns the name for this user.
+	 * If names are stored as parts "first",
+	 * "middle", "last" they should be
+	 * concatenated with spaces.
+	 *
+	 * @return string|null
+	 */
+	public function getName(): ?string
+	{
+		return null;
+	}
+
+	/**
+	 * Returns whether this user is eligible
+	 * for authentication.
+	 *
+	 * @return bool
+	 */
+	public function isActive(): bool
+	{
+		return empty($this->attributes['deleted_at']);
+	}
+}
